ece3e62759c4160de09e127781e618e7eb75451c
prem5285 <pk370068@gmail.com>
2022-04-02 19:34:20 +0530

    Modified GitSampleCucumber

--- src/main/java/com/gitsample/GitSampleCucumber.java
+++ src/main/java/com/gitsample/GitSampleCucumber.java
@@ -1,5 +1,19 @@
 package com.gitsample;
 
 public class GitSampleCucumber {
+	
+	
+	public void method1() {
+System.out.println("1");
+	}
+	public void method2() {
+System.out.println("2");
+	}
+	public void method3() {
+System.out.println("3");
+	}
+	private void method4() {
+System.out.println("4");
+	}
 
 }
